dc550173917eda95f98fee2cedf3f474497629a4
Nathan Roark <nathanroark@protonmail.com>
2024-01-16 13:59:08 -0600

    og correct

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Nathan Roark",
   url: "https://nathanroark.dev",
-  ogImage: "https://nathanroark.dev/og.png",
+  ogImage: "https://nathanroark.dev/og.png", // png is the original format, jpg is there because discord keeps linking to jpg
   description:
     "Full Stack Engineer passionate about building and learning new things.",
   mainNav: [
