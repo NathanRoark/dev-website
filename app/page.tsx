b6d88160ce5f94619bdd4b90285b5504c8fcba17
Nathan Roark <nathanroark@protonmail.com>
2024-01-16 19:59:02 -0600

    head room

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function Page() {
             </div>
           </div>
 
-          <Avatar className="size-28">
+          <Avatar className="ml-1 size-28">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
